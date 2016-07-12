1c329486b9d770e5a5c67ee5d476ea84897398bb
Michael Spallino <mikehspallino@gmail.com>
2016-07-12 17:48:40 -0400

    actually fixed rk4 this time

--- neutron_stars.py
+++ neutron_stars.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot
 
 
 def rk4(f, x0, y0, x_max, n):
-    dx = np.empty(n + 1)
-    dy = np.empty(n + 1)
+    dx = [0] * (n + 1)
+    dy = [0] * (n + 1)
     h = (x_max - x0) / float(n)
 
     dx[0] = x = x0
@@ -27,51 +27,31 @@ def rk4(f, x0, y0, x_max, n):
     return dx, dy
 
 
-def euler(f, x0, y0, x_max, n):
-    dx = np.empty(n + 1)
-    dy = np.empty(n + 1)
-    h = (x_max - x0) / float(n)
-
-    dx[0] = x = x0
-    dy[0] = y = y0
-    for i in range(n):
-        dy[i+1] = dy[i] + (h) * f(y, i)
-        y = dy[i]
-        print(y)
-        # print(dy[i])
-
-    return dx, dy
-
-
-def simple_dx_dt(x, t):
+def simple_dx_dt(t, x):
     """
     dx
-    -- = -t
+    -- = -x
     dt
     """
 
-    return -t
+    return -x
 
 
 def exact_dx_dt(x, t):
     """
-    x = e^(t)
+    x = e^(-t)
     """
-    return math.pow(math.e, x)
+    return math.pow(math.e, -t)
 
 
 def solve_rk4_dx_dt():
-    return rk4(exact_dx_dt, 0, 1, 10, 10)
-
-
-def solve_euler_dx_dt():
-    return euler(exact_dx_dt, 0, 1, 10, 10)
+    return rk4(simple_dx_dt, 0, 1, 10, 10)
 
 
 def solve_exact_dx_dt():
     y = np.empty(11)
-    for t in range(11):
-        y[t] = exact_dx_dt(t, y)
+    for t in range(0, 11, 1):
+        y[t] = exact_dx_dt(y, t)
         # print(y[t])
 
     return y
@@ -80,8 +60,8 @@ def solve_exact_dx_dt():
 if __name__ == '__main__':
     sol = solve_exact_dx_dt()
     rk4_sol = solve_rk4_dx_dt()
-    matplotlib.pyplot.plot(sol, 'b', label='e^(t)')
-    matplotlib.pyplot.plot(rk4_sol[1], 'r', label='rk4')
+    matplotlib.pyplot.plot(sol, 'b', label='e^(-t)')
+    matplotlib.pyplot.plot(rk4_sol[0], rk4_sol[1], 'r', label='rk4')
     matplotlib.pyplot.legend(loc='best')
     matplotlib.pyplot.xlabel('t')
     matplotlib.pyplot.grid()
